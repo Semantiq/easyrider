908237ddd6124b642183ff6ddf3a5c09e760ab14
Michal Jurczynski <michal.jurczynski@gmail.com>
2013-12-14 21:34:26 +0000

    Defining a dedicated actor to manage compilation (work in progress)

--- src/test/scala/eu/semantiq/easyrider/CompilerTest.scala
+++ src/test/scala/eu/semantiq/easyrider/CompilerTest.scala
@@ -17,7 +17,7 @@ class CompilerTest extends TestKit(ActorSystem("CompilerTest")) with ImplicitSen
     expectMsg(200.milliseconds, Compiler.CompilationSuccessful)
   }
   it("should safely abort compilation if working copy is updated during compilation") {
-    val compiler = system.actorOf(Compiler(testActor, workingCopy.location, Some("1sec.sh"), 2.seconds), "compiler-restart")
+    val compiler = system.actorOf(Compiler(testActor, workingCopy.location, Some("sh 1sec.sh"), 2.seconds), "compiler-restart")
 
     compiler ! Compile
     compiler ! Compile
@@ -25,13 +25,13 @@ class CompilerTest extends TestKit(ActorSystem("CompilerTest")) with ImplicitSen
     expectNoMsg(2.seconds)
   }
   it("should notify about compilation failures") {
-    val compiler = system.actorOf(Compiler(testActor, workingCopy.location, Some("failing.sh"), 2.seconds))
+    val compiler = system.actorOf(Compiler(testActor, workingCopy.location, Some("sh failing.sh"), 2.seconds))
 
     compiler ! Compile
     expectMsg(CompilationFailure)
   }
   it("should notify about compilation failure in case of timeout") {
-    val compiler = system.actorOf(Compiler(testActor, workingCopy.location, Some("10sec.sh"), 500.milliseconds))
+    val compiler = system.actorOf(Compiler(testActor, workingCopy.location, Some("sh 10sec.sh"), 500.milliseconds))
 
     compiler ! Compile
     expectMsg(CompilationFailure)
