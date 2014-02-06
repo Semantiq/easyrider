0a9db265d555366379f1eba953a8ec1046e1afbe
Michal Jurczynski <michal.jurczynski@gmail.com>
2014-02-06 19:20:16 +0000

    Using more explicit style for other actor's message types.

--- src/main/scala/eu/semantiq/easyrider/console/ApiController.scala
+++ src/main/scala/eu/semantiq/easyrider/console/ApiController.scala
@@ -1,32 +1,31 @@
 package eu.semantiq.easyrider.console
 
 import akka.actor.{ActorLogging, ActorRef, Props, Actor}
+import akka.util.ByteString
 import spray.can.server.websockets.Sockets
 import spray.can.server.websockets.model.{OpCode, Frame}
-import akka.util.ByteString
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
 import org.json4s.jackson.Serialization
 import org.json4s.jackson.Serialization.write
-import eu.semantiq.easyrider.StatusMonitor.{GetStatus, Status}
+import eu.semantiq.easyrider.StatusMonitor
 import eu.semantiq.easyrider.supervisor.AppSupervisor
-import AppSupervisor.AppLifecycleCommand
 
 // TODO: separate from WebSocket handling code
 class ApiController(statusMonitor: ActorRef) extends Actor with ActorLogging {
-  implicit val formats = Serialization.formats(FullTypeHints(List(classOf[AppLifecycleCommand])))
+  implicit val formats = Serialization.formats(FullTypeHints(List(classOf[AppSupervisor.AppLifecycleCommand])))
 
   def initializing: Receive = {
     case Sockets.Upgraded =>
       context.become(running(sender))
-      context.system.eventStream.subscribe(self, classOf[Status])
-      statusMonitor ! GetStatus
+      context.system.eventStream.subscribe(self, classOf[StatusMonitor.Status])
+      statusMonitor ! StatusMonitor.GetStatus
   }
 
   def running(peer: ActorRef): Receive = {
-    case event: Status => peer ! Frame(opcode = OpCode.Text, data = ByteString(write(event)))
+    case event: StatusMonitor.Status => peer ! Frame(opcode = OpCode.Text, data = ByteString(write(event)))
     case Frame(_, _, OpCode.Text, _, data) =>
-      val command = parse(data.utf8String).extract[AppLifecycleCommand]
+      val command = parse(data.utf8String).extract[AppSupervisor.AppLifecycleCommand]
       context.system.eventStream.publish(command)
   }
 
