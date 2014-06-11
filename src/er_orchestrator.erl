372bf006c596b766dbb06c04fa27ae459ad1fa28
Michal Jurczynski <michal.jurczynski@gmail.com>
2014-06-11 22:59:47 +0100

    New event handler for release manager and orchestrator.

--- src/er_orchestrator.erl
+++ src/er_orchestrator.erl
@@ -12,35 +12,18 @@ start_link() -> gen_server:start_link({global, ?MODULE}, ?MODULE, [], []).
 %% gen_server
 
 init(_Args) ->
-	er_release_manager:subscribe_recommended_versions(self()),
+	er_event_bus:subscribe(self(), [recommended_versions]),
 	{ok, {}}.
 
 handle_call(_Message, _From, State) -> {reply, ok, State}.
 
-handle_cast({recommended_version, {{AppName, StageName}, Version, immediate}}, State) ->
+handle_cast({event, recommended_versions, {{AppName, StageName}, {Version, immediate}}}, State) ->
 	io:format("Performing recommened deployment: ~p v ~p to ~p~n", [AppName, Version, StageName]),
-	{apps, Apps} = er_apps:apps(),
-	Instances = get_instances(Apps, AppName, StageName),
+	{snapshot, instances, AllInstances} = er_event_bus:get_snapshot(instances),
+	Instances = [ Instance || {{ThisAppName, ThisStageName, _Id}, Instance} <- AllInstances, ThisAppName == AppName, ThisStageName == StageName ],
 	[ er_node_agent:deploy_instance(Node, Id, Version, Props) || #instance{id = Id, node = Node, properties = Props} <- Instances ],
 	{noreply, State}.
 
-%% helpers
-
-get_instances(Apps, AppName, StageName) ->
-	case lists:keysearch(AppName, 2, Apps) of
-		% {value, App} ->
-		% 	Stages = App#app.stages,
-		% 	case lists:keysearch(StageName, 2, Stages) of
-		% 		{value, Stage} ->
-		% 			Stage#stage.instances;
-		% 		false ->
-		% 			io:format("Stage is not defined~n", []),
-		% 			[]
-		% 	end;
-		false ->
-			io:format("Application is not defined~n", []),
-			[]
-	end.
 %% other gen_server
 
 terminate(_, _) -> stub.
--- src/er_release_manager.erl
+++ src/er_release_manager.erl
@@ -2,64 +2,33 @@
 -behaviour(gen_server).
 -include("er_apps.hrl").
 -include("er_repository.hrl").
--export([start_link/0, subscribe_recommended_versions/1, get_recommended_versions/0]).
+-export([start_link/0]).
 -export([init/1, handle_call/3, handle_cast/2, terminate/2, code_change/3, handle_info/2]).
 
--record(state, {recommended_versions = undefined, versions = undefined, apps = undefined, subscriptions = []}).
+-record(state, {versions = undefined, apps = undefined, stages = undefined, subscriptions = []}).
 
 %% Interface
 
 % @private
 start_link() -> gen_server:start_link({global, ?MODULE}, ?MODULE, [], []).
 
-% @doc Subscribe to version recommendations.
-% Recommendations mean that it's safe to install given version of the AppName in given Stage immediately.
-% The subsciber get the updates in the form of:
-% {recommended_version,{{AppName,Stage},Version,mode()}}}
--spec subscribe_recommended_versions(Subscriber :: pid()) -> ok.
-subscribe_recommended_versions(Pid) -> gen_server:cast({global, ?MODULE}, {subscribe_recommended_versions, Pid}), ok.
-
-% @doc Get the list of recommended versions for each stage of each app.
-% Please note that recommendations may not always be available (for example when {@link er_repository} is down).
--spec get_recommended_versions() -> {recommended_versions, [{{AppName::string(), Stage::string()}, Version::string()}]}.
-get_recommended_versions() -> gen_server:call({global, ?MODULE}, {get_recommended_versions}).
-
 %% gen_server
 
 % @private
 init(_Args) ->
 	er_repository:subscribe_versions(self(), 1),
-	er_event_bus:subscribe(self(), [apps, stages]),
-	{ok, #state{}}.
+	{snapshot, apps, Apps} = er_event_bus:get_snapshot(apps),
+	{snapshot, stages, Stages} = er_event_bus:get_snapshot(stages),
+	{ok, #state{apps = Apps, stages = Stages}}.
 
-% @private
-handle_call({get_recommended_versions}, _From, State) ->
-	{reply, {recommended_versions, State#state.recommended_versions}, State}.
-
-% @private
-handle_cast({subscribe_recommended_versions, Pid}, State) ->
-	erlang:monitor(process, Pid),
-	{noreply, State#state{subscriptions = [Pid | State#state.subscriptions]}};
-% handle_cast({new_version, Version}, State) ->
-% 	AppName = Version#version_info.app,
-% 	Stages = case lists:keysearch(AppName, 2, State#state.apps) of
-% 		{value, App} -> [ Stage#stage.name || Stage <- App#app.stages ];
-% 		error -> []
-% 	end,	
-% 	Recommendations = [ {{AppName, Stage}, Version, immediate} || Stage <- Stages ],
-% 	[ notify(Recommendation, State) || Recommendation <- Recommendations ],
-% 	NewRecommendations = lists:foldl(fun({{_, Stage}, _, _}, NewRecommendations) ->
-% 		orddict:store({AppName, Stage}, Version, NewRecommendations)
-% 	end, State#state.recommended_versions, Recommendations),
-% 	{noreply, State#state{recommended_versions = NewRecommendations}};
-handle_cast({version_approved, _Version}, State) -> {noreply, State};
-handle_cast({versions, Versions}, State) -> {noreply, make_recommendations(State#state{versions = Versions})};
-handle_cast({snapshot, apps, Apps}, State) ->
-	% TODO: {noreply, make_recommendations(State#state{apps = Apps})}.
+handle_cast({new_version, Version}, State) ->
+	AppName = Version#version_info.app,
+	{ok, Stages} = [ Stage || {{ThisAppName, _StageName}, Stage} <- State#state.stages, ThisAppName == AppName ],
+	Recommendations = [ {recommended_versions, {AppName, StageName}, {Version, immediate}} || #stage{stage_name = StageName} <- Stages ],
+	[ er_er_event_bus:publish(Recommendation) || Recommendation <- Recommendations ],
 	{noreply, State};
-handle_cast({snapshot, stages, Stage}, State) -> {noreply, State};
-handle_cast({event, apps, AppName, App}, State) -> {noreply, State};
-handle_cast({event, stages, {AppName, StageName}, Stage}, State) -> {noreply, State}.
+handle_cast({version_approved, _Version}, State) -> {noreply, State};
+handle_cast({versions, Versions}, State) -> {noreply, make_recommendations(State#state{versions = Versions})}.
 
 % @private
 handle_info({'DOWN', _, process, Pid, _}, State) ->
@@ -67,7 +36,7 @@ handle_info({'DOWN', _, process, Pid, _}, State) ->
 
 %% helpers
 
-% @private
+% % @private
 % make_recommendations(State) when State#state.versions /=undefined, State#state.apps /= undefined ->
 % 	Recommendations = [
 % 		{{App#app.name, Stage#stage.name}, version_for(App#app.name, Stage#stage.name, State)}
@@ -75,17 +44,13 @@ handle_info({'DOWN', _, process, Pid, _}, State) ->
 % 	State#state{recommended_versions = Recommendations};
 make_recommendations(State) -> State.
 
-% @private
-version_for(App, _Stage, State) ->
-	Versions = State#state.versions,
-	case lists:keysearch(App, 1, Versions) of
-		{value, {_, [AppVersion | _]}} -> AppVersion#version_info.number;
-		_ -> undefined
-	end.
-
-% @private
-notify(Recommendation, State) ->
-	[ gen_server:cast(Subscriber, {recommended_version, Recommendation}) || Subscriber <- State#state.subscriptions ].
+% % @private
+% version_for(App, _Stage, State) ->
+% 	Versions = State#state.versions,
+% 	case lists:keysearch(App, 1, Versions) of
+% 		{value, {_, [AppVersion | _]}} -> AppVersion#version_info.number;
+% 		_ -> undefined
+% 	end.
 
 %% other gen_server
 
@@ -93,3 +58,5 @@ notify(Recommendation, State) ->
 code_change(_, _, _) -> stub.
 % @private
 terminate(_, _) -> stub.
+% @private
+handle_call(_, _, _) -> stub.
