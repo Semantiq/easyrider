4bb7eeb974cfa30a7ed6f579327b46e811a1da16
Michal Jurczynski <michal.jurczynski@gmail.com>
2014-08-25 18:09:38 +0100

    Displaying ssh nodes.

--- hardwareClient/src/main/web/pages/sshNodes/controller.js
+++ hardwareClient/src/main/web/pages/sshNodes/controller.js
@@ -2,6 +2,7 @@ app.config(["$routeProvider", function($routeProvider) {
 	$routeProvider.when("/sshNodes", {
 		templateUrl: "/pages/sshNodes/template.html",
 		controller: ["$scope", "Command", "SshNodes", function($scope, Command, SshNodes) {
+		    $scope.sshNodeConfigurationEvents = SshNodes.list;
 			$scope.addSshNode = function() {
 				Command.show(SshNodes.addSshNodeTemplate());
 			};
