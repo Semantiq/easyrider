b645765215e4e27540e041fd388378f00af7060a
Michal Jurczynski <michal.jurczynski@gmail.com>
2014-08-30 12:13:58 +0100

    Fake container starting and stopping, to enable front-end work.

--- hardwareClient/src/main/scala/easyrider/business/core/SshNodeAgent.scala
+++ hardwareClient/src/main/scala/easyrider/business/core/SshNodeAgent.scala
@@ -5,6 +5,7 @@ import akka.event.LoggingReceive
 import com.jcraft.jsch.{ChannelSftp, ChannelExec, JSch}
 import easyrider.Applications.ContainerId
 import easyrider.Infrastructure._
+import easyrider.Repository.Version
 import easyrider.SshInfrastructure.{NodeConfigurationUpdatedEvent, CreateNode, NodeConfiguration}
 import easyrider.{EventDetails, EventId, EventKey}
 import org.apache.commons.io.IOUtils
@@ -28,9 +29,9 @@ class SshNodeAgent(eventBus: ActorRef) extends Actor {
       println(output)
       exitStatus match {
         case 0 =>
-          eventBus ! ContainerStateChangedEvent(eventDetails, Created)
+          eventBus ! ContainerStateChangedEvent(eventDetails, ContainerCreated)
         case other =>
-          eventBus ! ContainerStateChangedEvent(eventDetails, CreationFailed)
+          eventBus ! ContainerStateChangedEvent(eventDetails, ContainerCreationFailed)
       }
     case DeployVersion(commandId, containerId, version) =>
       val eventKey = containerId.eventKey append version.number
@@ -49,10 +50,24 @@ class SshNodeAgent(eventBus: ActorRef) extends Actor {
       channel.disconnect()
       session.disconnect()
       eventBus ! VersionDeploymentProgressEvent(EventDetails(EventId.generate(), eventKey, Seq(commandId)), version, DeploymentCompleted)
+    case StartContainer(commandId, containerId, version) =>
+      // TODO: implement
+      runSshCommand(configuration, "echo '" + version.number + "' > " + containerDir(containerId) + "/running.version")
+      eventBus ! ContainerStateChangedEvent(EventDetails(EventId.generate(), containerId.eventKey, Seq(commandId)), ContainerRunning(version))
+    case StopContainer(commandId, containerId, immediate) =>
+      // TODO: implement
+      val (0, runningVersionNumber) = runSshCommand(configuration, "cat " + containerDir(containerId) + "/running.version")
+      eventBus ! ContainerStateChangedEvent(EventDetails(EventId.generate(), containerId.eventKey, Seq(commandId)), ContainerStopping(Version(containerId.stageId.applicationId, runningVersionNumber.trim)))
+      Thread.sleep(1000)
+      eventBus ! ContainerStateChangedEvent(EventDetails(EventId.generate(), containerId.eventKey, Seq(commandId)), ContainerCreated)
   }
 
-  private def versionsDir(containerId: ContainerId): String = {
-    s"easyrider/containers/${containerId.containerName}/versions"
+  private def versionsDir(containerId: ContainerId) = {
+    containerDir(containerId) + "/versions"
+  }
+
+  def containerDir(containerId: ContainerId) = {
+    s"easyrider/containers/${containerId.containerName}"
   }
 
   override def receive = unConfigured
--- hardwareClient/src/test/scala/easyrider/business/tests/SshInfrastructureManagementTest.scala
+++ hardwareClient/src/test/scala/easyrider/business/tests/SshInfrastructureManagementTest.scala
@@ -32,7 +32,7 @@ class SshInfrastructureManagementTest extends EasyriderTest(new Easyrider(8081, 
 
   }
 
-  it should "deploy application packages on a container on ssh host" in {
+  it should "deploy & start application packages on a container on ssh host" in {
     val client = TestProbe()
     val api = easyrider.actorSystem.actorOf(easyrider.core.apiFactory(client.ref))
 
@@ -41,14 +41,28 @@ class SshInfrastructureManagementTest extends EasyriderTest(new Easyrider(8081, 
 
     client.send(api, Subscribe(CommandId.generate(), "deploymentEvents", classOf[VersionDeploymentProgressEvent], EventKey()))
     client.expectMsgClass(classOf[Subscribed[_]])
+    client.send(api, Subscribe(CommandId.generate(), "containerState", classOf[ContainerStateChangedEvent], EventKey()))
+    client.expectMsgClass(classOf[Subscribed[_]])
 
     client.send(api, SshInfrastructure.CreateNode(CommandId.generate(), NodeConfiguration(NodeId("nodeA"), "localhost", 22, "test", "test")))
     client.send(api, Applications.CreateApplication(CommandId.generate(), Application(ApplicationId("app"), Seq())))
     client.send(api, Applications.CreateStage(CommandId.generate(), Stage(StageId(ApplicationId("app"), "qa"), Seq())))
     client.send(api, Applications.CreateContainerConfiguration(CommandId.generate(), ContainerConfiguration(ContainerId(StageId(ApplicationId("app"), "qa"), "0"), NodeId("nodeA"), Seq())))
+
+    client.expectMsgClass(classOf[ContainerStateChangedEvent]).state should be (ContainerCreated)
+
     client.send(api, Infrastructure.DeployVersion(CommandId.generate(), ContainerId(StageId(ApplicationId("app"), "qa"), "0"), Version(ApplicationId("app"), "1.0.0")))
 
     client.expectMsgClass(classOf[VersionDeploymentProgressEvent]).state should be (DeploymentInProgress)
     client.expectMsgClass(classOf[VersionDeploymentProgressEvent]).state should be (DeploymentCompleted)
+
+    client.send(api, Infrastructure.StartContainer(CommandId.generate(), ContainerId(StageId(ApplicationId("app"), "qa"), "0"), Version(ApplicationId("app"), "1.0.0")))
+
+    client.expectMsgClass(classOf[ContainerStateChangedEvent]).state should be (ContainerRunning(Version(ApplicationId("app"), "1.0.0")))
+
+    client.send(api, Infrastructure.StopContainer(CommandId.generate(), ContainerId(StageId(ApplicationId("app"), "qa"), "0")))
+
+    client.expectMsgClass(classOf[ContainerStateChangedEvent]).state should be (ContainerStopping(Version(ApplicationId("app"), "1.0.0")))
+    client.expectMsgClass(classOf[ContainerStateChangedEvent]).state should be (ContainerCreated)
   }
 }
