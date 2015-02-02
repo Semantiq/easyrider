a9f148d9174ad97b1c1291029a82b1ddc1811a48
Michal Jurczynski <michal.jurczynski@gmail.com>
2015-02-02 01:11:05 +0000

    Ssh node status using snapshot subscriptions.

--- ssh/src/main/web/extensions/SshNodes.js
+++ ssh/src/main/web/extensions/SshNodes.js
@@ -2,8 +2,7 @@ app.service('SshNodes', ['Api', 'Validators', 'Utils', function(Api, Validators,
 	var me = this;
 
 	me.configuration = Api.subscribeSnapshot("easyrider.business.ssh.SshInfrastructure$NodeConfiguration", function(config) {});
-	me.stateSubscription = Api.subscribe("easyrider.Infrastructure$NodeUpdatedEvent", []);
-	me.stateList = me.stateSubscription.snapshot;
+	me.nodeStates = Api.subscribeSnapshot("easyrider.Infrastructure$NodeState", function(state) {});
 
 	me.addSshNodeTemplate = function() {
 		return {
@@ -20,21 +19,6 @@ app.service('SshNodes', ['Api', 'Validators', 'Utils', function(Api, Validators,
 		};
 	};
 
-	me.nodeStates = function() {
-        var nodes = {
-        };
-        for (var i in me.stateList) {
-            nodes[me.stateList[i].eventDetails.eventKey.key[0]] = me.stateList[i];
-        }
-        return nodes;
-	};
-//	me.removeApplicationTemplate = function(applicationId) {
-//		return {
-//			jsonClass: "easyrider.Applications$RemoveApplication",
-//			applicationId: applicationId
-//		};
-//	};
-//
 	Validators.addValidator("easyrider.business.ssh.SshInfrastructure$CreateNode",
 		function(command, reporter) {
 		if(!Utils.isValidId(command.nodeConfiguration.id.id))
